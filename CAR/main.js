1e2503a35e0ba19223fe080644ded46c02f8bcb2
Ahmed Khaled Mohamed <100778273+Ahmed-Khaled-JS@users.noreply.github.com>
2024-05-08 11:11:35 +0300

    Add files via upload

---
+++ CAR/main.js
@@ -0,0 +1,13 @@
+function acceptRequest() {
+    var consumerName = document.getElementById("consumer-name").value;
+    var carNumber = document.getElementById("car-number").value;
+    var numberOfDays = document.getElementById("number-of-days").value;
+    alert("Your accept is successful");
+}
+
+function rejectRequest() {
+    var consumerName = document.getElementById("consumer-name").value;
+    var carNumber = document.getElementById("car-number").value;
+    var numberOfDays = document.getElementById("number-of-days").value;
+    alert("Your reject is successful");
+}
---
+++ ownerHome.js
@@ -0,0 +1,18 @@
+
+
+function AddCar(){
+
+    window.location.href="/addVechile.html"
+
+}
+
+function showMycars(){
+
+    window.location.href="/vehicles.html"
+
+}
+
+function showMyrequests(){
+    window.location.href="/request.html"
+
+}
